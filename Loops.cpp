1e7b623b312a11b134187e3e02c5355976b37d48
Vinegar-Picks <ymancilla0623@gmail.com>
2026-09-22 10:41:28 -0500

    Add project files.

---
+++ Loops.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //int n = 10;
+    //for (int i=1; i<=n; i++)
+    //{
+    //    cout << i << "  " << (n-(i-1)) << endl;
+    //}
+
+    //while (1<=10)
+    //{
+    //cout << i << endl;
+    //1++
+    //}
+
+    //int i = 100;
+    //do
+    //{
+    //    cout << i << endl;
+    //    i++;
+    //} while (i <= 10);
+    cout << "Table of 5" << endl;
+    cout << "----------" << endl;
+    for (int i = 1; i<=12; i++)
+    {
+        cout << "5 x " << i << " = " << (5 * i) << endl;
+    }
+}
